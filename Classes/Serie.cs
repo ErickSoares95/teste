8876e86b9c0ae50bb37dc1b1c9d45345c977f3a4
ErickSoares95 <ericklira99@gmail.com>
2021-09-24 18:32:44 -0300

    API-SERIES

---
+++ Classes/Serie.cs
@@ -0,0 +1,75 @@
+﻿using AV1.Series;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AV1.Series
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+
+        private string Titulo { get; set; }
+
+        private string Descricao { get; set; }
+
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Genero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Genero + Environment.NewLine;
+            retorno += "Descrição:  " + this.Genero + Environment.NewLine;
+            retorno += "Ano de Inicio: " + this.Genero + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+
+        public Genero retornaGenero()
+        {
+            return Genero;
+        }
+
+        public string retornaDescricao()
+        {
+            return Descricao;
+        }
+
+        public int retornaAno()
+        {
+            return Ano;
+        }
+    }
+}
---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,42 @@
+﻿using AV1.Series.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AV1.Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+	{
+		private List<Serie> listaSerie = new List<Serie>();
+		public void Atualiza(int id, Serie objeto)
+		{
+			listaSerie[id] = objeto;
+		}
+
+		public void Exclui(int id)
+		{
+			listaSerie[id].Excluir();
+		}
+
+		public void Insere(Serie objeto)
+		{
+			listaSerie.Add(objeto);
+		}
+
+		public List<Serie> Lista()
+		{
+			return listaSerie;
+		}
+
+		public int ProximoId()
+		{
+			return listaSerie.Count;
+		}
+
+		public Serie RetornaPorId(int id)
+		{
+			return listaSerie[id];
+		}
+	}
+}
